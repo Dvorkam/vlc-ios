7d3dec0c87c41f5a0e0436ddcbaf9b671a753458
Pushpinder Pal Singh <pushpinderpal19@gmail.com>
2021-07-03 16:00:56 +0530

    Add MediaContinueWatchingCell initialization code

--- Sources/MediaCategoryCells/MediaContinueWatchingCollectionViewCell.swift
+++ Sources/MediaCategoryCells/MediaContinueWatchingCollectionViewCell.swift
@@ -13,10 +13,30 @@ class MediaContinueWatchingCollectionViewCell: BaseCollectionViewCell {
 	@IBOutlet var thumbnailView: UIImageView!
 	@IBOutlet var titleLabel: VLCMarqueeLabel!
 	@IBOutlet var timeLeftLabel: VLCMarqueeLabel!
-	
+
+	override var media: VLCMLObject? {
+		didSet {
+			if let movie = media as? VLCMLMedia {
+				titleLabel.text = movie.title()
+				accessibilityLabel = movie.accessibilityText(editing: false)
+				thumbnailView.layer.cornerRadius = 3
+				thumbnailView.image = movie.thumbnailImage()
+			}
+		}
+	}
+
+    private var enableMarquee: Bool {
+        return !UserDefaults.standard.bool(forKey: kVLCSettingEnableMediaCellTextScrolling)
+    }
+
 	override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
+		if #available(iOS 11.0, *) {
+			thumbnailView.accessibilityIgnoresInvertColors = true
+		}
+		titleLabel.labelize = enableMarquee
+		titleLabel?.textColor = PresentationTheme.current.colors.cellTextColor
+	}
 
 }
