4db9d2b96484c06baf539b0588dfcb4969832b76
EshanSingh-ES <eeeshan789@gmail.com>
2024-06-16 04:57:02 +0530

    MetaDataEditorView: optimise for iPad

--- Sources/Media Library/Editing/MetaDataEditorView.swift
+++ Sources/Media Library/Editing/MetaDataEditorView.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2024 VideoLAN. All rights reserved.
 //
 
-
 import Foundation
 import UIKit
 
@@ -23,21 +22,21 @@ class MetaDataEditorView: UIView {
         self.textField = UITextField()
         super.init(frame: .zero)
         setupView()
+        setupOrientationObserver()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
     private func setupView() {
-        
         label.text = returnLabelText(meta: metaDataType)
-      
-        if UIDevice.current.orientation == .portrait {
-            label.font = UIFont.italicSystemFont(ofSize:20)
-        } else {
-            label.font = UIFont.italicSystemFont(ofSize: 10)
-        }
+        
+        updateLabelFont()
         
         label.translatesAutoresizingMaskIntoConstraints = false
         
@@ -48,41 +47,53 @@ class MetaDataEditorView: UIView {
         
         addSubview(label)
         addSubview(textField)
-        setupOrientationObserver()
         setupConstraints()
     }
     
+    private func updateLabelFont() {
+        let device = UIDevice.current
+        let isIpad = device.userInterfaceIdiom == .pad
+        
+        if device.orientation == .portrait || device.orientation == .portraitUpsideDown {
+            label.font = isIpad ? UIFont.italicSystemFont(ofSize: 30) : UIFont.italicSystemFont(ofSize: 20)
+        } else {
+            label.font = isIpad ? UIFont.italicSystemFont(ofSize: 20) : UIFont.italicSystemFont(ofSize: 10)
+        }
+    }
+    
     private func setupOrientationObserver() {
-          NotificationCenter.default.addObserver(self, selector: #selector(handleOrientationChange), name: UIDevice.orientationDidChangeNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(handleOrientationChange), name: UIDevice.orientationDidChangeNotification, object: nil)
     }
-       
+    
     @objc private func handleOrientationChange() {
-          setupConstraints()
-          setupView()
+        DispatchQueue.main.async {
+            self.updateLabelFont()
+            self.setupConstraints()
+        }
     }
-       
+    
     private func setupConstraints() {
-           NSLayoutConstraint.deactivate(self.constraints)
-           
-           if UIDevice.current.orientation.isPortrait {
-               NSLayoutConstraint.activate([
-                   label.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
-                   label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-                   label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-                   textField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
-                   textField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-                   textField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-               ])
-           } else {
-               NSLayoutConstraint.activate([
-                   label.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
-                   label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-                   label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-                   textField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 3),
-                   textField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
-                   textField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
-               ])
-           }
+        NSLayoutConstraint.deactivate(self.constraints)
+        
+        if !UIDevice.current.orientation.isLandscape {
+            NSLayoutConstraint.activate([
+                label.topAnchor.constraint(equalTo: self.topAnchor, constant: 20),
+                label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+                textField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 20),
+                textField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                textField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            ])
+        } else {
+            NSLayoutConstraint.activate([
+                label.topAnchor.constraint(equalTo: self.topAnchor, constant: 10),
+                label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                label.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+                textField.topAnchor.constraint(equalTo: label.bottomAnchor, constant: 3),
+                textField.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20),
+                textField.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -20),
+            ])
+        }
     }
     
     private func returnLabelText(meta: MetaDataType) -> String {
@@ -95,7 +106,6 @@ class MetaDataEditorView: UIView {
             return "Song's Artist Name:"
         case .genre:
             return "Song's Genre Name:"
-            
         }
     }
 }
@@ -104,6 +114,5 @@ extension MetaDataEditorView: UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         delegate?.metaDataNewInfo(meta: metaDataType, value: textField.text ?? "")
     }
-    
-    
 }
+
