91d95906194e9b6a078a6656bf53ef51577809a3
Mohamed Sliem <mohamedhu99@gmail.com>
2024-09-07 23:05:04 +0300

    MiniPlayerInfoCollectionViewFlowLayout: Update the layout

--- Sources/Playback/Player/MiniPlayer-iOS/MiniPlayerInfoCollectionViewFlowLayout.swift
+++ Sources/Playback/Player/MiniPlayer-iOS/MiniPlayerInfoCollectionViewFlowLayout.swift
@@ -9,7 +9,6 @@
 import UIKit
 
 class MiniPlayerInfoCollectionViewFlowLayout: UICollectionViewFlowLayout {
-    var previousPage: CGFloat = 0
     
     override func prepare() {
         super.prepare()
@@ -20,32 +19,5 @@ class MiniPlayerInfoCollectionViewFlowLayout: UICollectionViewFlowLayout {
         guard let itemSize = self.collectionView?.frame.size else { return }
         self.itemSize = itemSize
     }
-
-    override func targetContentOffset(forProposedContentOffset proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
-
-        guard let collectionView = self.collectionView else {
-            return super.targetContentOffset(forProposedContentOffset: proposedContentOffset, withScrollingVelocity: velocity)
-        }
-
-        let pageWidth = self.itemSize.width + self.minimumInteritemSpacing
-        let approximatePage = collectionView.contentOffset.x / pageWidth
-
-        let currentPage: CGFloat
-        if velocity.x == 0 {
-            currentPage = round(approximatePage)
-        } else if velocity.x < -0.4 {
-            currentPage = floor(approximatePage)
-        } else if velocity.x > 0.4 {
-            currentPage = ceil(approximatePage)
-        } else {
-            currentPage = previousPage
-        }
-
-        previousPage = currentPage
-        
-        let newHorizontalOffset = (currentPage * pageWidth) - collectionView.contentInset.left
-        return CGPoint(x: newHorizontalOffset, y: proposedContentOffset.y)
-    }
-    
 }
 
