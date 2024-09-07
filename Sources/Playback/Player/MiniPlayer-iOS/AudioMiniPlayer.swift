2e62c0a18cd8782bf4cc1c1bda223a44efa8e6bb
Mohamed Sliem <mohamedhu99@gmail.com>
2024-09-07 23:06:05 +0300

    AudioMiniPlayer: Update Media Info Collection View
    This should fix these issues, wrong current info for the shuffled list and the media info cell size in the landscape mode.
    Add functionality for infinite scrolling when RepeatAllItems mode is
    active with handling also the previous action for infinite scrolling.
    add limitation in dragging or declarating for only one cell per the
    snap.

--- Sources/Playback/Player/MiniPlayer-iOS/AudioMiniPlayer.swift
+++ Sources/Playback/Player/MiniPlayer-iOS/AudioMiniPlayer.swift
@@ -56,14 +56,28 @@ class AudioMiniPlayer: UIView, MiniPlayer, QueueViewControllerDelegate {
 
     @IBOutlet weak var stopOverlay: UIView!
     @IBOutlet weak var stopImageView: UIImageView!
-
+    
+    private var previousMediaBounceIcon: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "MiniPrev")
+        imageView.tintColor = .white
+        return imageView
+    }()
+    
     private let draggingDelegate: MiniPlayerDraggingDelegate
 
     private let animationDuration = 0.2
 
     private var mediaService: MediaLibraryService
     private lazy var playbackService = PlaybackService.sharedInstance()
+    private var mediaList: VLCMediaList {
+        playbackService.isShuffleMode ? playbackService.shuffledList : playbackService.mediaList
+    }
 
+    private var isRepeatAllMode: Bool {
+       return playbackService.repeatMode == .repeatAllItems
+    }
+    
     private var queueViewController: QueueViewController?
 
     var position = MiniPlayerPosition(vertical: .bottom, horizontal: .center)
@@ -71,7 +85,9 @@ class AudioMiniPlayer: UIView, MiniPlayer, QueueViewControllerDelegate {
     var tapticPosition = MiniPlayerPosition(vertical: .bottom, horizontal: .center)
     var panDirection: PanDirection = .vertical
 
-    var mediaInfoCollectionViewContentOffset: CGPoint = .zero
+    //Media Info CollectinView Offset Info
+    var scrollOffsetBeforeDragging: CGPoint = .zero
+    var scrollOffsetAfterDecelerating: CGPoint = .zero
 
     var stopGestureEnabled: Bool {
         if #available(iOS 13.0, *) {
@@ -88,12 +104,27 @@ class AudioMiniPlayer: UIView, MiniPlayer, QueueViewControllerDelegate {
         initView()
         setupConstraint()
         setupMediaInfoCollectionView()
+        setupPreviousMediaBounceImage()
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        //Update the cell size when orientation changed
+        DispatchQueue.main.async {
+            guard let flowLayout = self.mediaInfoCollectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
+                return
+            }
+            flowLayout.itemSize = self.mediaInfoCollectionView.frame.size
+            flowLayout.invalidateLayout()
+            self.mediaInfoCollectionView.invalidateIntrinsicContentSize()
+            self.updateMediaInfoIndex()
+        }
+    }
+    
     func updatePlayPauseButton() {
         playPauseButton.isSelected = playbackService.isPlaying
     }
@@ -219,7 +250,10 @@ extension AudioMiniPlayer: VLCPlaybackServiceDelegate {
         if !playbackService.isPlayingOnExternalScreen() && !playbackService.playAsAudio {
             playbackService.videoOutputView = artworkImageView
         }
-        updateMediaInfoCollectionView()
+        
+        updateMediaInfoIndex()
+        playModeUpdated()
+        mediaInfoCollectionView.reloadData()
     }
 
     func mediaPlayerStateChanged(_ currentState: VLCMediaPlayerState,
@@ -253,6 +287,15 @@ extension AudioMiniPlayer: VLCPlaybackServiceDelegate {
         }
 
         queueViewController.reload()
+    }
+    
+    func playModeUpdated() {
+        if isRepeatAllMode {
+            mediaInfoCollectionView.bounces = true
+        } else {
+            mediaInfoCollectionView.bounces = false
+        }
+        mediaInfoCollectionView.reloadData()
     }
 }
 
@@ -503,16 +546,21 @@ private extension AudioMiniPlayer {
         }
     }
 
-    private func updateMediaInfoCollectionView() {
+    //Update or set media info collection view index with the currently playing media
+    private func updateMediaInfoIndex() {
         guard let media = playbackService.currentlyPlayingMedia else { return }
-        let mediaIndex = playbackService.mediaList.index(of: media)
+        var mediaIndex: UInt
+        mediaIndex = mediaList.index(of: media)
+        
         guard let currentCell = mediaInfoCollectionView.visibleCells.first,
         let currentIndex = mediaInfoCollectionView.indexPath(for: currentCell) else { return }
 
         if abs(Int(mediaIndex) - currentIndex.row) == 1 {
-            mediaInfoCollectionView.scrollToItem(at: IndexPath(row: Int(mediaIndex), section: 0), at: .centeredHorizontally, animated: true)
+            mediaInfoCollectionView.scrollToItem(at: IndexPath(row: Int(mediaIndex), section: 0),
+                                                 at: .centeredHorizontally, animated: true)
         } else {
-            mediaInfoCollectionView.scrollToItem(at: IndexPath(row: Int(mediaIndex), section: 0), at: .centeredHorizontally, animated: false)
+            mediaInfoCollectionView.scrollToItem(at: IndexPath(row: Int(mediaIndex), section: 0),
+                                                 at: .centeredHorizontally, animated: false)
         }
     }
 }
@@ -605,7 +653,7 @@ extension AudioMiniPlayer: UIContextMenuInteractionDelegate {
     }
 }
 
-// MARK: - Media Info CollectionView Delegate
+// MARK: - MiniPLayer Media Info CollectionView Delegate
 
 extension AudioMiniPlayer: UICollectionViewDelegate, UICollectionViewDataSource {
     func numberOfSections(in collectionView: UICollectionView) -> Int {
@@ -613,16 +661,25 @@ extension AudioMiniPlayer: UICollectionViewDelegate, UICollectionViewDataSource 
     }
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return playbackService.mediaList.count
+        // adding a cell to the count for buffering to achieve infinite scrolling in RepeatAllMode
+        return isRepeatAllMode ? mediaList.count + 1 : mediaList.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MiniPlayerInfoCollectionViewCell", for: indexPath) as? MiniPlayerInfoCollectionViewCell else {
+        guard let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "MiniPlayerInfoCollectionViewCell",
+            for: indexPath
+        ) as? MiniPlayerInfoCollectionViewCell else {
             fatalError("Couldn't load MiniPlayerInfoCollectionViewCell")
         }
-
-        let indexMedia = playbackService.mediaList.media(at: UInt(indexPath.row))
+        
+        let index: UInt = isRepeatAllMode
+                  ? UInt(indexPath.row % mediaList.count)
+                  : UInt(indexPath.row)
+        
+        let indexMedia = mediaList.media(at: index)
         cell.configure(for: indexMedia)
+
         return cell
     }
     
@@ -632,24 +689,121 @@ extension AudioMiniPlayer: UICollectionViewDelegate, UICollectionViewDataSource 
 
 extension AudioMiniPlayer: UIScrollViewDelegate {
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        mediaInfoCollectionViewContentOffset = scrollView.contentOffset
+        scrollOffsetBeforeDragging = scrollView.contentOffset
     }
 
     func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-        if !decelerate {
+        if !decelerate && mediaInfoCollectionView.contentOffset.x >= 0 {
+            handlePreviousNextAction()
+        }
+    }
+
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        let contentWidth = scrollView.contentSize.width
+        let scrollViewWidth = scrollView.frame.size.width
+        let offsetX = scrollView.contentOffset.x
+        
+        let isBouncingOnLeftEdge = (scrollOffsetAfterDecelerating.x < -25)
+      
+        if isRepeatAllMode && isBouncingOnLeftEdge {
+            handlePreviousNextAction(forcePrevious: true)
+        } else if mediaInfoCollectionView.contentOffset.x >= 0 {
             handlePreviousNextAction()
         }
     }
     
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        handlePreviousNextAction()
+    func scrollViewWillEndDragging(
+        _ scrollView: UIScrollView,
+        withVelocity velocity: CGPoint,
+        targetContentOffset: UnsafeMutablePointer<CGPoint>
+    ) {
+        let proposedOffset = targetContentOffset.pointee.x
+        let previousOffset = scrollOffsetBeforeDragging
+        let flowLayout = mediaInfoCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        let cellWidth = flowLayout.itemSize.width + flowLayout.minimumLineSpacing
+        
+        // current offset before scrolling end.
+        //This helper for determining if there is bouncing happened.
+        scrollOffsetAfterDecelerating = scrollView.contentOffset
+
+        // Check if scrolling to the next or previous cell satisfies the width ratio.
+        guard abs(proposedOffset - scrollOffsetBeforeDragging.x) > (0.4 * cellWidth) else {
+            targetContentOffset.pointee = previousOffset
+            return
+        }
+
+        let newOffset = calculateNewScrollOffset(
+            velocity: velocity.x,
+            proposedOffset: proposedOffset,
+            cellWidth: cellWidth
+        )
+
+        targetContentOffset.pointee = CGPoint(x: newOffset, y: targetContentOffset.pointee.y)
+    }
+    
+    // Calculate the new offset for scrolling
+    private func calculateNewScrollOffset(
+        velocity: CGFloat,
+        proposedOffset: CGFloat,
+        cellWidth: CGFloat
+    ) -> CGFloat {
+        var newOffset: CGFloat = scrollOffsetBeforeDragging.x
+
+        if (velocity == 0 && proposedOffset > 0) || abs(velocity) < 0.4 {
+            newOffset += proposedOffset > scrollOffsetBeforeDragging.x ? cellWidth : -cellWidth
+        }
+
+        if velocity > 0.4 {
+            newOffset += cellWidth
+        } else if velocity < -0.4 {
+            newOffset -= cellWidth
+        }
+
+        return newOffset
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetX = scrollView.contentOffset.x
+        // Check if the user scrolls beyond the left (first cell)
+        if offsetX < 0 {
+            updatePreviousMediaBounceImage(alpha: abs(offsetX / 60))
+        }
+    }
+}
+
+// MARK: - Handler for Previous / Next Action through the Audio Mini Player
+
+extension AudioMiniPlayer {
+    private func setupPreviousMediaBounceImage() {
+        previousMediaBounceIcon.image = UIImage(named: "MiniPrev")
+        
+        previousMediaBounceIcon.frame = CGRect(
+            x: -45, y: mediaInfoCollectionView.frame.height / 2 - 17.5,
+            width: 35, height: 35)
+        
+        mediaInfoCollectionView.addSubview(previousMediaBounceIcon)
+    }
+    
+    func updatePreviousMediaBounceImage(alpha: CGFloat = 0.0) {
+        previousMediaBounceIcon.alpha = alpha
+    }
+    
+    private func handlePreviousNextAction(forcePrevious: Bool = false) {
+        let finalContentOffset = mediaInfoCollectionView.contentOffset
+        // Normal Mode
+        if finalContentOffset.x > scrollOffsetBeforeDragging.x {
+            playbackService.next()
+        } else if finalContentOffset.x < scrollOffsetBeforeDragging.x || forcePrevious {
+            playbackService.previousMedia()
+        }
     }
 
-    private func handlePreviousNextAction() {
+    private func handlePreviousNextRepeatOnRepeatAll() {
         let finalContentOffset = mediaInfoCollectionView.contentOffset
-        if finalContentOffset.x > mediaInfoCollectionViewContentOffset.x {
+
+        if finalContentOffset.x > (mediaInfoCollectionView.contentSize.width) {
             playbackService.next()
-        } else if finalContentOffset.x < mediaInfoCollectionViewContentOffset.x {
+        } else {
             playbackService.previousMedia()
         }
     }
