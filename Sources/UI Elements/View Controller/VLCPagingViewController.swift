26cbc4c1e5ec35e6f9ecbdfc36d1bb2bb3aff8c7
Diogo Simao Marques <dogo@videolabs.io>
2025-02-11 16:26:46 +0100

    VLCPagingViewController: Update the constraints
    The buttonBarView's constraints are now aligned with the safe area
    witout the use of left and right anchors.
    
    The containerView's bottom constraint is now aligned with the view's
    bottom anchor to properly show the container over the safe area since
    the bottom bar is hidden.

--- Sources/UI Elements/View Controller/VLCPagingViewController.swift
+++ Sources/UI Elements/View Controller/VLCPagingViewController.swift
@@ -74,19 +74,19 @@ class VLCPagingViewController<ButtonBarCellType: UICollectionViewCell>: PagerTab
         buttonBarView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(buttonBarView)
         NSLayoutConstraint.activate([
-            buttonBarView.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor),
-            buttonBarView.rightAnchor.constraint(equalTo: view.rightAnchor),
-            buttonBarView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            buttonBarView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            buttonBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            buttonBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             buttonBarView.heightAnchor.constraint(equalToConstant: buttonbarViewHeight)
-            ])
+        ])
+
         // make sure that top and bottom are not covered by tabbar and navigationbar
         NSLayoutConstraint.activate([
             containerView.topAnchor.constraint(equalTo: buttonBarView.bottomAnchor),
             containerView.rightAnchor.constraint(equalTo: view.rightAnchor),
             containerView.leftAnchor.constraint(equalTo: view.leftAnchor),
-            containerView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
-            ]
-        )
+            containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
 
         buttonBarView.delegate = self
         buttonBarView.dataSource = self
